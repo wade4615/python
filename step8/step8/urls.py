992074d5480bf7cffbba3da91504047a972f052c
WADEOS <wade4615@gmail.com>
2020-09-03 12:40:22 -0500

    updated projects

--- step8/step8/urls.py
+++ step8/step8/urls.py
@@ -1,4 +1,4 @@
-"""config URL Configuration
+"""step8 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
--- step8/step8/wsgi.py
+++ step8/step8/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for config project.
+WSGI step8 for step8 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'step8.settings')
 
 application = get_wsgi_application()
