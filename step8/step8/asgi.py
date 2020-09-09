2d919fa520284be6e132bae812abb67c32989b71
WADEOS <wade4615@gmail.com>
2020-09-09 09:40:12 -0500

    update to basic workings of step8

--- step8/step8/asgi.py
+++ step8/step8/asgi.py
@@ -1,10 +1,10 @@
 """
-ASGI config for step8 project.
+ASGI step8 for step8 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+https://docs.djangoproject.com/en/dev/howto/deployment/asgi/
 """
 
 import os
--- step8/step8/wsgi.py
+++ step8/step8/wsgi.py
@@ -1,10 +1,10 @@
 """
-WSGI config for step8 project.
+WSGI step8 for step8 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
 
 import os
